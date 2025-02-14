34b03c9165eaebc378ec3da6a0ff63c2b510d010
pgomezgonzalez <p.gomez1424@gmail.com>
2025-02-14 15:52:38 +0000

    fix Feb14

--- barcode_pipeline/ont_barcodes_analysis.py
+++ barcode_pipeline/ont_barcodes_analysis.py
@@ -136,7 +136,7 @@ def cli():
 	sp.run(r'''cat list_bams | parallel -j 1 "samtools view ./mapping/{}.bam | wc -l >> total_reads"''', shell=True)
 	sp.run(r'''cat list_bams | parallel -j 1 "samtools view -F4 ./mapping/{}.bam | wc -l >> mapped_reads"''', shell=True)
 	sp.run(r'''cat list_bams | parallel -j 1 "samtools view -f4 ./mapping/{}.bam | wc -l >> unmapped_reads"''', shell=True)
-	sp.run(r'paste list_bams total_reads mapped_reads unammaped_reads > table_number_reads', shell=True)
+	sp.run(r'paste list_bams total_reads mapped_reads unmapped_reads > table_number_reads', shell=True)
 
 
 	############################################################################################################################################################
@@ -151,7 +151,7 @@ def cli():
 
 	##create bam files excluding unmapped reads 
 	print("...creating mapped bam files...")
-	sp.run(r'''cat list_bams | parallel -j 1 "samtools view -F4 ./mapping/{}.bam > ./mapping/{}.mapped.bam"''',shell=True)
+	sp.run(r'''cat list_bams | parallel -j 1 "samtools view -F4 ./mapping/{}.bam -b -o ./mapping/{}.mapped.bam"''',shell=True)
 	
 	print("...counting reads...")
 
